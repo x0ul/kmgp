679fcbd771b8dda8046c17f33358deed155ab4f7
Cody Harris <cody@hypodyne.net>
2022-04-18 19:16:14 -0700

    got some show (as opposed to episode) work done, need a checkpoint

--- back-end/scheduler/scheduler.py
+++ back-end/scheduler/scheduler.py
@@ -16,16 +16,29 @@ bp = Blueprint("scheduler", __name__)
 
 
 @bp.route("/")
+@login_required
 def index():
-    """Show all the episodes, most recent first."""
+    """Show all the shows and episodes, most recent first."""
+    # TODO don't get old ones
     db = get_db()
-    # TODO do the join thing
-    episodes = db.execute(
-        "SELECT e.id, show_id, title, air_date, url, description, created_by, updated_by, created_at, updated_at"
-        " FROM Episodes e"
-        " ORDER BY air_date DESC"
-    ).fetchall()
-    return render_template("scheduler/index.html", episodes=episodes)
+    shows = db.execute(
+        "SELECT *"
+        " FROM Shows"
+        " JOIN UserShowsJoin ON UserShowsJoin.user_id = ? and UserShowsJoin.show_id = Shows.id"
+        " ORDER BY created_at DESC"
+        , (g.user["id"],)).fetchall()
+
+    episodes = []
+    for show in shows:
+        episodes = db.execute(
+            "SELECT *"
+            " FROM Episodes"
+            " WHERE show_id = ?"
+            " ORDER BY air_date DESC",
+            (show["id"],),
+        ).fetchall()
+
+    return render_template("scheduler/index.html", shows=shows, episodes=episodes)
 
 
 def get_episode(id):
@@ -59,9 +72,111 @@ def get_episode(id):
     return post
 
 
-@bp.route("/create", methods=("GET", "POST"))
+def get_show(id):
+    """Get show by id.
+
+    TODO use the join table to check if the current auth'd user is an owner.
+
+    :param id: id of episode to get
+    TODO :param check_user: require the current user to be an owner
+    :return: the episode
+    :raise 404: if an episode with the given id doesn't exist
+    :raise 403: if the current user isn't an owner
+    """
+    post = (
+        get_db()
+        .execute(
+            "SELECT e.id, show_id, air_date, url, created_at, updated_at, description, title"
+            " FROM Episodes e"
+            " WHERE e.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post["author_id"] != g.user["id"]:
+        abort(403)
+
+    return post
+
+
+def get_episode(id):
+    """Get an episode by id.
+
+    TODO use the join table to check if the current auth'd user is an owner.
+
+    :param id: id of episode to get
+    TODO :param check_user: require the current user to be an owner
+    :return: the episode
+    :raise 404: if an episode with the given id doesn't exist
+    :raise 403: if the current user isn't an owner
+    """
+    post = (
+        get_db()
+        .execute(
+            "SELECT e.id, show_id, air_date, url, created_at, updated_at, description, title"
+            " FROM Episodes e"
+            " WHERE e.id = ?",
+            (id,),
+        )
+        .fetchone()
+    )
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post["author_id"] != g.user["id"]:
+        abort(403)
+
+    return post
+
+
+
+@bp.route("/shows/create", methods=("GET", "POST"))
 @login_required
-def create():
+def create_show():
+    """Create a new post for the current user."""
+    db = get_db()
+    if request.method == "GET":
+        djs = db.execute(
+            "SELECT id, name"
+            " FROM Users"
+            " WHERE id != ?",
+            (g.user["id"],)).fetchall()
+        return render_template("scheduler/create_show.html", djs=djs)
+
+    if request.method == "POST":
+        title = request.form["title"]
+        description = request.form["description"]
+        error = None
+
+        if not title:
+            error = "Title is required."
+
+        if error:
+            flash(error)
+        else:
+            show_id = db.execute(
+                "INSERT INTO Shows (title, description, created_by, updated_by)"
+                " VALUES (?, ?, ?, ?)"
+                " RETURNING id",
+                (title, description, g.user["id"], g.user["id"]),
+            ).fetchone()["id"]
+            print(f"show_id: {show_id}")
+            db.execute(
+                "INSERT INTO UserShowsJoin (user_id, show_id)"
+                " VALUES (?, ?)",
+                (g.user["id"], show_id))
+            db.commit()
+            return redirect(url_for("scheduler.index"))
+
+
+@bp.route("/episodes/create", methods=("GET", "POST"))
+@login_required
+def create_episode():
     """Create a new post for the current user."""
     if request.method == "POST":
         title = request.form["title"]
@@ -90,12 +205,43 @@ def create():
             db.commit()
             return redirect(url_for("scheduler.index"))
 
-    return render_template("scheduler/create.html")
+    return render_template("scheduler/create_episode.html")
 
 
-@bp.route("/<int:id>/update", methods=("GET", "POST"))
+@bp.route("/shows/<int:id>/update", methods=("GET", "POST"))
 @login_required
-def update(id):
+def update_show(id):
+    """Update a show."""
+    post = get_episode(id)
+
+    if request.method == "POST":
+        title = request.form["title"]
+        description = request.form["description"]
+        air_date = request.form["air_date"]
+        audio_file = request.form["audio_file"]
+        error = None
+
+        air_date = datetime.strptime(air_date, "%Y-%m-%dT%H:%M")
+
+        if not title:
+            error = "Title is required."
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                "UPDATE post SET title = ?, air_date = ?, description = ? WHERE id = ?", (title, air_date, description, id)
+            )
+            db.commit()
+            return redirect(url_for("scheduler.index"))
+
+    return render_template("scheduler/update_show.html", post=post)
+
+
+@bp.route("/episodes/<int:id>/update", methods=("GET", "POST"))
+@login_required
+def update_episode(id):
     """Update a post if the current user is the author."""
     post = get_episode(id)
 
@@ -121,12 +267,12 @@ def update(id):
             db.commit()
             return redirect(url_for("scheduler.index"))
 
-    return render_template("scheduler/update.html", post=post)
+    return render_template("scheduler/update_episode.html", post=post)
 
 
-@bp.route("/<int:id>/delete", methods=("POST",))
+@bp.route("/episodes/<int:id>/delete", methods=("POST",))
 @login_required
-def delete(id):
+def delete_episode(id):
     """Delete a post.
 
     Ensures that the post exists and that the logged in user is the
