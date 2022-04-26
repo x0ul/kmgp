08fb168c273d5c032838e6db60981d9e0324c7fb
Cody Harris <cody@hypodyne.net>
2022-04-26 11:50:47 -0700

    perhaps this will work with heroku postgres

---
+++ scheduler/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+
+class Config:
+    """
+    Populate values from env or a .env file (via python-dotenv).
+
+    Values set in the .env file will not override env vars.
+    """
+    load_dotenv()
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DATABASE_URL = os.environ.get('DATABASE_URL')
--- scheduler/db.py
+++ scheduler/db.py
@@ -1,5 +1,3 @@
-import sqlite3
-
 import psycopg2
 from psycopg2.extras import DictCursor
 
@@ -15,16 +13,9 @@ def get_db():
     again.
     """
     if "db" not in g:
-        g.db = psycopg2.connect("dbname=kmgp user=kmgp", cursor_factory=DictCursor)
+        g.db = psycopg2.connect("postgres:///kmgp?user=kmgp", cursor_factory=DictCursor)
+        print(f"database uri: {current_app.config['DATABASE_URL']}")
     return g.db
-
-    # if "db" not in g:
-    #     g.db = sqlite3.connect(
-    #         current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
-    #     )
-    #     g.db.row_factory = sqlite3.Row
-
-    # return g.db
 
 
 def close_db(e=None):
@@ -39,8 +30,6 @@ def close_db(e=None):
 
 def init_db():
     """Clear existing data and create new tables."""
-    #db = get_db()
-
     with current_app.open_resource("schema.sql") as f, get_db() as db:
         cur = db.cursor()
         cur.execute(f.read().decode("utf8"))
