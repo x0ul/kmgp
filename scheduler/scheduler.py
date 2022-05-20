ed78254a86efdf982705128476a671c865b1f23f
Cody Harris <cody@hypodyne.net>
2022-05-19 22:40:24 -0700

    rename show -> episode (things are probably broken)

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -22,7 +22,7 @@ bp = Blueprint("scheduler", __name__)
 @bp.route("/")
 @login_required
 def index():
-    """Show all the programs and shows, most recent first."""
+    """Show all the programs and episodes, most recent first."""
     # TODO don't get old ones
     db = get_db()
     cur = db.cursor()
@@ -38,23 +38,23 @@ def index():
         (g.user["id"],))
     programs = cur.fetchall()
 
-    shows = {}
+    episodes = {}
     for program in programs:
         cur.execute(
-            "SELECT s.id, s.title, s.air_date, s.description, s.created_at, s.updated_at,"
+            "SELECT e.id, e.title, e.air_date, e.description, e.created_at, e.updated_at,"
             " creator.name as creator, updater.name as updater"
-            " FROM Shows s"
-            " JOIN Users creator ON s.created_by = creator.id"
-            " JOIN Users updater on s.updated_by = updater.id"
-            " WHERE s.program_id = %s"
-            " AND s.air_date >= CURRENT_TIMESTAMP"
+            " FROM Episodes e"
+            " JOIN Users creator ON e.created_by = creator.id"
+            " JOIN Users updater on e.updated_by = updater.id"
+            " WHERE e.program_id = %s"
+            " AND e.air_date >= CURRENT_TIMESTAMP"
             " ORDER BY air_date DESC",
             (program["id"],),
         )
-        shows[program["id"]] = cur.fetchall()
+        episodes[program["id"]] = cur.fetchall()
 
 
-    return render_template("scheduler/index.html", programs=programs, shows=shows)
+    return render_template("scheduler/index.html", programs=programs, episodes=episodes)
 
 
 def get_other_djs(my_id):
@@ -115,31 +115,31 @@ def get_program(id):
     return program
 
 
-def get_show(id):
-    """Get an show by id.
+def get_episode(id):
+    """Get an episode by id.
 
     TODO use the join table to check if the current auth'd user is an owner.
 
-    :param id: id of show to get
+    :param id: id of episode to get
     TODO :param check_user: require the current user to be an owner
-    :return: the show
-    :raise 404: if an show with the given id doesn't exist
+    :return: the episode
+    :raise 404: if an episode with the given id doesn't exist
     :raise 403: if the current user isn't an owner
     """
     db = get_db()
     cur = db.cursor()
     cur.execute(
         "SELECT e.id, program_id, air_date, url, created_at, updated_at, description, title"
-        " FROM Shows e"
+        " FROM Episodes e"
         " WHERE e.id = %s",
         (id,))
 
-    show = cur.fetchone()
+    episode = cur.fetchone()
 
-    if not show:
-        abort(404, f"Show id {id} doesn't exist.")
+    if not episode:
+        abort(404, f"episode id {id} doesn't exist.")
 
-    return show
+    return episode
 
 
 @bp.route("/programs/create", methods=("GET", "POST"))
@@ -192,9 +192,9 @@ def create_program():
     return render_template("scheduler/create_program.html", djs=djs)
 
 
-@bp.route("/programs/<int:id>/create_show", methods=("GET", "POST"))
+@bp.route("/programs/<int:id>/create_episode", methods=("GET", "POST"))
 @login_required
-def create_show(id):
+def create_episode(id):
     """Create a new post for the current user."""
     db = get_db()
     cur = db.cursor()
@@ -226,14 +226,14 @@ def create_show(id):
             flash(error)
         else:
             cur.execute(
-                "INSERT INTO Shows (program_id, title, air_date, url, description, created_by, updated_by)"
+                "INSERT INTO Episodes (program_id, title, air_date, url, description, created_by, updated_by)"
                 " VALUES (%s, %s, %s, %s, %s, %s, %s)",
                 (id, title, air_date, "http://example.com", description, g.user["id"], g.user["id"]),  # TODO url of audio file
             )
             db.commit()
             return redirect(url_for("scheduler.index"))
 
-    return render_template("scheduler/create_show.html", program=program, upload=upload)
+    return render_template("scheduler/create_episode.html", program=program, upload=upload)
 
 
 @bp.route("/programs/<int:id>/update", methods=("GET", "POST"))
@@ -267,10 +267,10 @@ def update_program(id):
 
 
 # TODO the file
-@bp.route("/shows/<int:id>/update", methods=("GET", "POST"))
+@bp.route("/episodes/<int:id>/update", methods=("GET", "POST"))
 @login_required
-def update_show(id):
-    show = get_show(id)
+def update_episode(id):
+    episode = get_episode(id)
 
     if request.method == "POST":
         title = request.form["title"]
@@ -290,7 +290,7 @@ def update_show(id):
             db = get_db()
             cur = db.cursor()
             cur.execute(
-                "UPDATE Shows"
+                "UPDATE Episodes"
                 " SET title = %s, air_date = %s, description = %s, updated_at = CURRENT_TIMESTAMP, updated_by = %s"
                 " WHERE id = %s",
                 (title, air_date, description, id, g.user["id"])
@@ -298,20 +298,20 @@ def update_show(id):
             db.commit()
             return redirect(url_for("scheduler.index"))
 
-    return render_template("scheduler/update_show.html", show=show)
+    return render_template("scheduler/update_episode.html", episode=episode)
 
 
-@bp.route("/shows/<int:id>/delete", methods=("POST",))
+@bp.route("/episodes/<int:id>/delete", methods=("POST",))
 @login_required
-def delete_show(id):
+def delete_episode(id):
     """Delete a post.
 
     Ensures that the post exists and that the logged in user is the
     author of the post.
     """
-    get_show(id)
+    get_episode(id)
     db = get_db()
     cur = db.cursor()
-    cur.execute("DELETE FROM Shows WHERE id = %s", (id,))
+    cur.execute("DELETE FROM Episodes WHERE id = %s", (id,))
     db.commit()
     return redirect(url_for("scheduler.index"))
